e21f6c6010f31570ad4f056e7aabb9222d7f5eb5
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-09-25 03:13:27 +0100

    Added some changes to the card component

--- src/Components/ChannelCard.jsx
+++ src/Components/ChannelCard.jsx
@@ -1,3 +1,49 @@
-export default function ChannelCard() {
-  return <div>ChannelCard</div>;
+import { Link } from 'react-router-dom';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import { demoProfilePicture } from '../utils/constants';
+
+export default function ChannelCard({ channelDetail }) {
+  return (
+    <Box sx={{ boxShadow: 'none', borderRadius: '20px' }}>
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignContent: 'center',
+            color: '#fff',
+          }}
+        >
+          <CardMedia
+            sx={{
+              borderRadius: '50%',
+              height: '180px',
+              width: '180px',
+              mb: 2,
+              border: '1px solid #e3e3e3',
+            }}
+            image={
+              channelDetail?.snippet?.thumbnails?.high?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetail?.snippet?.title}
+          />
+          <Typography variant='h6'>
+            {channelDetail?.snippet?.title}{' '}
+            <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: 5 }} />
+          </Typography>
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(
+                channelDetail?.statistics?.subscriberCount
+              ).toLocaleString()}
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
+  );
 }
--- src/Components/VideoCard.jsx
+++ src/Components/VideoCard.jsx
@@ -18,7 +18,13 @@ export default function VideoCard({
   },
 }) {
   return (
-    <Card>
+    <Card
+      sx={{
+        width: { md: '320px', xs: '100%' },
+        boxShadow: 'none',
+        borderRadius: '0',
+      }}
+    >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           alt={snippet?.title}
@@ -30,6 +36,18 @@ export default function VideoCard({
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
           <Typography variant='subtitle1' fontWeight='bold' color='#fff'>
             {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant='subtitle2' fontWeight='bold' color='gray'>
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: 5 }} />
           </Typography>
         </Link>
       </CardContent>
